852552aae8b4ee814ac2765bed489e1d65c8015e
Sergey Krivov <sergey@krivov.info>
2018-10-09 12:23:33 +0300

    truffle configuration for deployment

---
+++ migrations/2_deploy_RootsDealRoomFactory.js
@@ -0,0 +1,10 @@
+var RootsDealRoomFactory = artifacts.require("./RootsDealRoomFactory.sol");
+
+const configs = require('./../configs/config');
+
+module.exports = function(deployer) {
+  deployer.deploy(
+      RootsDealRoomFactory,
+      configs.tokenAddress
+  );
+};
